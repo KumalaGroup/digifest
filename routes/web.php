76bda217c6b0aea97351420e20e7eae5fdf34769
Gaza <gazachmad@gmail.com>
2020-10-14 14:34:03 +0800

    update route laravel 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\User\Home;
+use App\Http\Controllers\User\LineUp;
+use App\Http\Controllers\User\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,17 +17,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 //Route Admin
-Route::match(['get', 'post'], '/masuk', 'User\Login@index')->name('login');
-Route::match(['get', 'post'], '/daftar', 'User\Login@daftar')->name('daftar');
+Route::match(['get', 'post'], '/masuk', [Login::class, 'index'])->name('login');
+Route::match(['get', 'post'], '/daftar', [Login::class, 'daftar'])->name('daftar');
 
-Route::get('/', 'User\Home@index')->name('home');
-Route::match(['get', 'post'], '/profil', 'User\Home@profil')->name('profil');
-Route::get('/transaksi', 'User\Home@logout')->name('transaksi');
-Route::get('/keluar', 'User\Home@logout')->name('logout');
+Route::get('/', [Home::class, 'index'])->name('home');
+Route::match(['get', 'post'], '/profil', [Home::class, 'profil'])->name('profil');
+Route::get('/transaksi', [Home::class, 'logout'])->name('transaksi');
+Route::get('/keluar', [Home::class, 'logout'])->name('logout');
 
-Route::get('/{brand}', 'User\LineUp@index')->name('lineUp');
-Route::get('/{brand}/{detail}', 'User\LineUp@detail')->name('lineUpDetail');
+Route::get('/{brand}', [LineUp::class, 'index'])->name('lineUp');
+Route::get('/{brand}/{detail}', [LineUp::class, 'detail'])->name('lineUpDetail');
 
-Route::get('/{brand}/{detail}/interiorExterior', 'User\LineUp@interiorExterior')->name('interiorExterior');
-Route::get('/{brand}/{detail}/testDrive', 'User\LineUp@testDrive')->name('testDrive');
-Route::get('/{brand}/{detail}/penawaran', 'User\LineUp@penawaran')->name('penawaran');
+Route::get('/{brand}/{detail}/interiorExterior', [LineUp::class, 'interiorExterior'])->name('interiorExterior');
+Route::get('/{brand}/{detail}/testDrive', [LineUp::class, 'testDrive'])->name('testDrive');
+Route::get('/{brand}/{detail}/penawaran', [LineUp::class, 'penawaran'])->name('penawaran');
